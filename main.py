218db1e6043647d920d608d13a8d008a78b253df
Badica Ioana Maria <117526248+BadicaIoana@users.noreply.github.com>
2023-01-04 20:31:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
+# importing customtkinter
 import customtkinter as ctk
 
 
+# Create class BMI
 class BMI:
     def __init__(self):
+        
+        # Setting the colors of the window 
         ctk.set_appearance_mode('dark')
         ctk.set_default_color_theme('green')
-
+        
+        # Creating the window
         self.window = ctk.CTk()
         self.window.title('BMI Calculator')
         self.window.geometry('335x235')
         self.window.config(padx=20, pady=20)
+        
+        # Resizing the window
         self.window.columnconfigure(0, weight = 1, minsize = 50)
         self.window.columnconfigure(1, weight = 1, minsize = 50)
         self.window.rowconfigure(1, weight = 1, minsize = 50)
@@ -17,29 +24,36 @@ class BMI:
         self.window.rowconfigure(3, weight = 1, minsize = 50)
         self.window.rowconfigure(5, weight = 1, minsize = 50)
         
+        # Creating the label
         self.weight_label = ctk.CTkLabel(master=self.window, text='Weight(kg):')
         self.height_label = ctk.CTkLabel(master=self.window, text='Height(cm):')
         
+        # Placing the label
         self.weight_label.grid(column=0, row=2)
         self.height_label.grid(column=0, row=1)
         
+       # Creating the entry fields
         self.weight_entry = ctk.CTkEntry(master = self.window, corner_radius=5)
         self.height_entry = ctk.CTkEntry(master=self.window, corner_radius=5)
-
+        
+        # Placing the entry fields
         self.weight_entry.grid(column = 1, row = 2)
         self.height_entry.grid(column=1, row=1)
-
+        
+        # Set and place the submit button
         self.submit_btn = ctk.CTkButton(master=self.window, text='Submit', command=self.calculate)
         self.submit_btn.grid(column=1, row=3)
-
+        
+        # Displaying the result message
         self.result_var = ctk.StringVar(value='')
         self.result_label = ctk.CTkLabel(master=self.window, textvariable=self.result_var, font=('Arial', 10),
                                          text_color ='white')
-       
+       # Placing the result message
         self.result_label.grid(column=1, row=5)
 
         self.window.mainloop()
-
+        
+# Creating the function that calculate your BMI display the result and the solution of the problem
     def calculate(self):
         weight = float(self.weight_entry.get())
         height = float(self.height_entry.get())
